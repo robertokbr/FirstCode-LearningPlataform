02acef5364a0fbfdf32663090c9abc4818f98569
ROBERT ROCHA DA COSTA JUNIOR <TETEIO@MacBook-Air-de-katlhen.local>
2020-08-06 02:38:56 -0300

    Add application Routes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import Landing from './pages/Lading';
 import './assets/styles/global.css';
+import Routes from "./Routes"
 
 const App: React.FC = () => (
-  <>
+  <Routes>
     <Landing />
-  </>
+  </Routes>
 );
 
 export default App;
