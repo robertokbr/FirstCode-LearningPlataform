1b1ed2766b03c5c8109028db0e9a600f1fc58959
ROBERT ROCHA DA COSTA JUNIOR <TETEIO@MacBook-Air-de-katlhen.local>
2020-08-06 02:39:39 -0300

    Add navigation into Home page

--- frontend/src/pages/Lading/index.tsx
+++ frontend/src/pages/Lading/index.tsx
@@ -5,6 +5,7 @@ import landingImg from '../../assets/images/landing.svg';
 import studyIcon from '../../assets/images/icons/study.svg';
 import giveClassIcon from '../../assets/images/icons/give-classes.svg';
 import purpleHeartIcon from '../../assets/images/icons/purple-heart.svg';
+import { Link } from 'react-router-dom';
 
 const Landing: React.FC = () => {
   return (
@@ -20,14 +21,14 @@ const Landing: React.FC = () => {
           className="hero-image"
         />
         <div className="buttons-container">
-          <a href="/" className="study">
+          <Link to="/study" className="study">
             <img src={studyIcon} alt="Estudar" />
             Estudar
-          </a>
-          <a href="/" className="give-classes">
+          </Link>
+          <Link to="/give-classes" className="give-classes">
             <img src={giveClassIcon} alt="Estudar" />
             Dar aulas
-          </a>
+          </Link>
         </div>
         <span className="total-connections">
           Total de 200 conexões já realizadas
