534ead28325ad7370cd70c3989459704a552e748
ROBERT ROCHA DA COSTA JUNIOR <TETEIO@MacBook-Air-de-katlhen.local>
2020-08-06 04:03:06 -0300

    Create Component Header

---
+++ frontend/src/components/PageHeader/index.tsx
@@ -0,0 +1,28 @@
+import React from "react";
+import {Link} from "react-router-dom";
+import {} from "react-icons/fi";
+import logoImg from "../../assets/images/logo.svg";
+import backIcon from "../../assets/images/icons/back.svg";
+
+interface HeaderProps{
+  title: string;
+}
+const PageHeader : React.FC<HeaderProps> = ({title,children})=>{
+  return (
+    <header className="page-header">
+    <div className="top-bar-container">
+    <Link to="/">
+    <img src={backIcon} alt="voltar"/>
+    </Link>
+    <img src={logoImg} alt="Proffy"/>
+    </div>
+    
+    <div className="header-content">
+    <strong>{title}</strong>
+    {children}
+    </div>
+    </header>
+    )
+  };
+  
+  export default PageHeader;
