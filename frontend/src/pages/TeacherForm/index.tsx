bfa161ac0b040bc3c44510a69a6ad28f44f61556
ROBERT ROCHA DA COSTA JUNIOR <TETEIO@MacBook-Air-de-katlhen.local>
2020-08-06 04:03:54 -0300

    Alter estruct page

--- frontend/src/pages/TeacherForm/index.tsx
+++ frontend/src/pages/TeacherForm/index.tsx
@@ -1,29 +1,14 @@
 import React from "react";
-import {Link} from "react-router-dom";
-import {} from "react-icons/fi";
-import logoImg from "../../assets/images/logo.svg";
-import backIcon from "../../assets/images/icons/back.svg";
-import "./styles.css";
+import PageHeader from "../../components/PageHeader";
+import "./styles.css"
 
 const TeacherForm : React.FC = ()=>{
   
   return (
-    
     <div id="page-teacher-list" className="container">
-    <header className="page-header">
-    <div className="top-bar-container">
-    <Link to="/">
-    <img src={backIcon} alt="voltar"/>
-    </Link>
-    <img src={logoImg} alt="Proffy"/>
-    </div>
-    
-    <div className="header-content">
-    <strong>Estes sao os proffys disponiveis.</strong>
-    </div>
-    </header>
+    <PageHeader title="Que incrivel que voce quer dar aula" />
     </div>
     )
   };
-  
+   
   export default TeacherForm;
