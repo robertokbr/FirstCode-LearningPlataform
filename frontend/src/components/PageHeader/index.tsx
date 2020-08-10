3994fe1192be25bd026e5f6a3823855f1dacc1b7
Roberto Junior <robertojuniordev@gmail.com>
2020-08-10 16:35:30 -0300

    Alter pageHeader configuration

--- frontend/src/components/PageHeader/index.tsx
+++ frontend/src/components/PageHeader/index.tsx
@@ -1,30 +1,30 @@
-import React from "react";
-import {Link} from "react-router-dom";
-import {} from "react-icons/fi";
-import logoImg from "../../assets/images/logo.svg";
-import backIcon from "../../assets/images/icons/back.svg";
+import React from 'react';
+import { Link } from 'react-router-dom';
+import {} from 'react-icons/fi';
+import logoImg from '../../assets/images/logo.svg';
+import backIcon from '../../assets/images/icons/back.svg';
 
 import './styles.css';
 
-interface HeaderProps{
+interface HeaderProps {
   title: string;
 }
-const PageHeader : React.FC<HeaderProps> = ({title,children})=>{
+const PageHeader: React.FC<HeaderProps> = ({ title, children }) => {
   return (
     <header className="page-header">
-    <div className="top-bar-container">
-    <Link to="/">
-    <img src={backIcon} alt="voltar"/>
-    </Link>
-    <img src={logoImg} alt="Proffy"/>
-    </div>
-    
-    <div className="header-content">
-    <strong>{title}</strong>
-    {children}
-    </div>
+      <div className="top-bar-container">
+        <Link to="/">
+          <img src={backIcon} alt="voltar" />
+        </Link>
+        <img src={logoImg} alt="Proffy" />
+      </div>
+
+      <div className="header-content">
+        <strong>{title}</strong>
+        {children}
+      </div>
     </header>
-    )
-  };
-  
-  export default PageHeader;
+  );
+};
+
+export default PageHeader;
