ba385eeba46b3571b52d8c1bd15b8c04cdebe380
Roberto Junior <robertojuniordev@gmail.com>
2020-08-10 20:25:24 -0300

    Create Class Index method

--- backend/src/controllers/ClassesController.ts
+++ backend/src/controllers/ClassesController.ts
@@ -17,12 +17,20 @@ export default class ClassesController {
       });
     }
     const subject = filters.subject as string;
-    const week_day = filters.subject as string;
+    const week_day = filters.week_day as string;
     const time = filters.time as string;
 
     const timeInMinutes = convertHoursToMinutes(filters.time as string);
 
     const classes = await db('classes')
+      .whereExists(function () {
+        this.select('class_schedule.*')
+          .from('class_schedule')
+          .whereRaw('`class_schedule`.`class_id` = `classes`.`id`')
+          .whereRaw('`class_schedule`.`week_day` = ??', [Number(week_day)])
+          .whereRaw('`class_schedule`.`from`  <= ??', [timeInMinutes])
+          .whereRaw('`class_schedule`.`to`  > ??', [timeInMinutes]);
+      })
       .where('classes.subject', '=', subject)
       .join('users', 'classes.user_id', '=', 'users.id')
       .select(['users.*', 'classes.*']);
