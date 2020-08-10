0e579d3e90b83a966d6eebe313e70a012aee7109
Roberto Junior <robertojuniordev@gmail.com>
2020-08-10 16:36:02 -0300

    Alter Landing  estructure

--- frontend/src/pages/Lading/index.tsx
+++ frontend/src/pages/Lading/index.tsx
@@ -1,42 +1,53 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import { FiLinkedin } from 'react-icons/fi';
 import './styles.css';
+
 import logoImg from '../../assets/images/logo.svg';
 import landingImg from '../../assets/images/landing.svg';
 import studyIcon from '../../assets/images/icons/study.svg';
-import giveClassIcon from '../../assets/images/icons/give-classes.svg';
+import giveClassesIcon from '../../assets/images/icons/give-classes.svg';
 import purpleHeartIcon from '../../assets/images/icons/purple-heart.svg';
-import { Link } from 'react-router-dom';
 
-const Landing: React.FC = () => {
+export default function Landing() {
   return (
     <div id="page-landing">
-      <div id="page-landing-content" className="container">
+      <div className="container" id="page-landing-content">
         <div className="logo-container">
-          <img src={logoImg} alt="FirstCode" />
-          <h2>Sua plataforma de estudo online</h2>
+          <img src={logoImg} alt="Proffy" />
+          <h2>
+            Sua Plataforma de estudos online
+            <br />
+            Para aprender programação
+            <br />
+            desde o primeiro código 🥇
+          </h2>
         </div>
+
         <img
           src={landingImg}
-          alt="Plataforma de estudos"
+          alt="Plataforma de Estudos"
           className="hero-image"
         />
+
         <div className="buttons-container">
           <Link to="/study" className="study">
             <img src={studyIcon} alt="Estudar" />
             Estudar
           </Link>
           <Link to="/give-classes" className="give-classes">
-            <img src={giveClassIcon} alt="Estudar" />
-            Dar aulas
+            <img src={giveClassesIcon} alt="Dar Aulas" />
+            Dar Aulas
           </Link>
         </div>
+
         <span className="total-connections">
-          Total de 200 conexões já realizadas
-          <img src={purpleHeartIcon} alt="Coração roxo" />
+          <a href="https://www.linkedin.com/in/robertojrcdc/">
+            A Roberto Junior's plataform.
+            <FiLinkedin />
+          </a>
         </span>
       </div>
     </div>
   );
-};
-
-export default Landing;
+}
