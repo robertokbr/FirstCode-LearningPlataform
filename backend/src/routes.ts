35906cdc80c6fca505d43997cd1ee864db5a2e43
Roberto Junior <robertojuniordev@gmail.com>
2020-08-11 15:35:39 -0300

    Create Connections Route

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -1,11 +1,15 @@
 import { Router } from 'express';
 import db from './database/connection';
 import ClassesController from './controllers/ClassesController';
+import ConnectionController from './controllers/ConnectionsController';
 
 const routes = Router();
 
 const classesController = new ClassesController();
-
+const connectionsController = new ConnectionController();
+routes.get('/connections', connectionsController.index);
+routes.post('/connections', connectionsController.create);
+routes.get('/classes', classesController.index);
 routes.post('/classes', classesController.create);
 
 routes.get('/user', async (request, response) => {
