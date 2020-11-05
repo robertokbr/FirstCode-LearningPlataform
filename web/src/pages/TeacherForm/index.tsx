3c74a270ac35e191065a185dd3b7a93c6a2796ad
robertokbr <robertojuniordev@gmail.com>
2020-11-05 13:34:13 -0300

    Change Select component options

--- web/src/pages/TeacherForm/index.tsx
+++ web/src/pages/TeacherForm/index.tsx
@@ -1,84 +1,53 @@
-import React from "react";
+import React from 'react';
+import './styles.css';
+import PageHeader from '../../components/PageHeader';
+import Input from '../../components/Input';
+import warningImg from '../../assets/images/icons/warning.svg';
+import TextArea from '../../components/TextArea';
+import Select from '../../components/Select';
 
-import "./styles.css";
-import PageHeader from "../../components/PageHeader/";
-import Input from "../../components/Input/";
-import Textarea from "../../components/Textarea/";
-import Select from "../../components/Select/";
-
-import warningIcon from "../../assets/images/icons/warning.svg";
-
-export default function TeacherForm() {
+const TeacherForm: React.FC = () => {
   return (
-    <div className="container" id="page-teacher-form">
+    <div id="page-teacher-form" className="container">
       <PageHeader
-        title="Que incrível que você quer dar aulas"
-        description="O primeiro passo é preencher esse formulario de inscrição."
+        title="Que incrivel que você quer dar aulas."
+        description="O primeiro passo é preencher esse formulário de escrição"
       />
-
       <main>
         <fieldset>
-          <legend>Seus Dados</legend>
-          <Input name="name" label="Nome Completo" />
+          <legend>Seus dados</legend>
+          <Input name="name" label="Nome completo" />
           <Input name="avatar" label="Avatar" />
-          <Input name="whatsapp" label="Whatsapp" />
-          <Textarea name="bio" label="Biografia" />
+          <Input name="whatsapp" label="Whatsapp" type="tel" />
+          <TextArea name="bio" label="Biografia" />
         </fieldset>
-
         <fieldset>
           <legend>Sobre a aula</legend>
           <Select
             name="subject"
             label="Matéria"
             options={[
-              { value: "Artes", label: "Artes" },
-              { value: "Biologia", label: "Biologia" },
-              { value: "Geografia", label: "Geografia" },
-              { value: "Matematica", label: "Matematica" },
-              { value: "Ingles", label: "Ingles" },
-              { value: "Historia", label: "Historia" },
-              { value: "Fisica", label: "Fisica" },
-              { value: "Portugues", label: "Portugues" },
-              { value: "Quimica", label: "Quimica" },
+              { value: 'Frontend', label: 'Frontend' },
+              { value: 'Backend', label: 'Artes' },
+              { value: 'Mobile', label: 'Mobile' },
+              { value: 'Devops', label: 'Devops' },
+              { value: 'FullStack', label: 'FullStack' },
             ]}
           />
-          <Input name="cost" label="Custo da sua hora por aula" />
+          <Input name="cost" label="Custo da usa hora por aula" type="text" />
         </fieldset>
-
-        <fieldset>
-          <legend>
-            Horários Disponiveis
-            <button type="button">+ Novo Horario</button>
-          </legend>
-
-          <div className="schedule-item">
-            <Select
-              name="week_day"
-              label="Dia da Semana"
-              options={[
-                { value: "0", label: "Domingo" },
-                { value: "1", label: "Segunda" },
-                { value: "2", label: "Terça" },
-                { value: "3", label: "Quarta" },
-                { value: "4", label: "Quinta" },
-                { value: "5", label: "Sexta" },
-                { value: "6", label: "Sábado" },
-              ]}
-            />
-            <Input name="from" label="Das" type="time" />
-            <Input name="to" label="Até" type="time" />
-          </div>
-        </fieldset>
-
         <footer>
           <p>
-            <img src={warningIcon} alt="Aviso Importante" />
-            Importante <br />
+            <img src={warningImg} alt="Warning" />
+            Importante!
+            <br />
             Preencha todos os dados
           </p>
-          <button type="button">Salvar cadastro</button>
+          <button type="button">Salvar cadástro</button>
         </footer>
       </main>
     </div>
   );
-}
+};
+
+export default TeacherForm;
